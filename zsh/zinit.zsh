c358f2b8bd9e6463248eb93d243cf0f4503f56e9
David O'Trakoun <me@davidosomething.com>
2023-06-12 16:17:56 -0400

    fix jq source

--- zsh/zinit.zsh
+++ zsh/zinit.zsh
@@ -88,7 +88,7 @@ function {
     '@sharkdp/fd' \
     \
     mv'jq* -> jq' \
-    'stedolan/jq' \
+    'jqlang/jq' \
     \
     pick'zoxide/zoxide' \
     atload'eval "$(zoxide init --cmd j zsh)"' \
