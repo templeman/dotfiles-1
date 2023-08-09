3336924b2f0efab883961b05dde0d894036a0fb7
Sam Templeman <sam.a.templeman@gmail.com>
2023-08-09 08:48:21 -0700

    Set spaces for html

--- nvim/ftplugin/html.lua
+++ nvim/ftplugin/html.lua
@@ -7,3 +7,6 @@ vim.g.html_indent_script1 = "zero"
 vim.g.html_indent_style1 = "zero"
 -- Don't indent first child of these tags
 vim.g.html_indent_autotags = "html,head,body"
+
+vim.bo.shiftwidth = 4
+vim.bo.expandtab = true
