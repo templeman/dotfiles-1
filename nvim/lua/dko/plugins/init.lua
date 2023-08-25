214cfee0ce6848a1db775ba0bffc1db70623b3e1
David O'Trakoun <me@davidosomething.com>
2023-08-24 23:30:46 -0400

    typings for tool configs

--- nvim/lua/dko/plugins/init.lua
+++ nvim/lua/dko/plugins/init.lua
@@ -299,8 +299,9 @@ return {
 
   -- package diff
   {
-    "vuki656/package-info.nvim",
+    "davidosomething/package-info.nvim",
     dependencies = { "MunifTanjim/nui.nvim" },
+    dev = true,
     event = { "BufReadPost package.json" },
     config = function()
       require("package-info").setup()
