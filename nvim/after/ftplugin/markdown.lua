8ac04ed1c3fa2b149ee45d51cd75a0efe768c1f3
Sam Templeman <sam.a.templeman@gmail.com>
2024-01-19 15:13:05 -0800

    Softwrap markdown files (useful with ZenMode)

---
+++ nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,8 @@
+-- Use in conjunction with ZenMode.nvim to wrap line length for readability.
+-- See also https://github.com/folke/zen-mode.nvim/issues/64 for the option to
+-- toggle soft wrapping in conjunction with ZenMode.nvim.
+-- vim.opt_local.textwidth = 0
+-- vim.opt_local.wrapmargin = 0
+vim.opt_local.wrap = true
+-- vim.opt_local.number = true
+vim.opt_local.linebreak = true
