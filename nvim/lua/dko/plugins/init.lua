4cc0e7dbed880e6ea2a102776b7fd7504fe36404
Sam Templeman <sam.a.templeman@gmail.com>
2024-01-22 09:36:27 -0800

    Noice not playing nice

--- nvim/lua/dko/plugins/init.lua
+++ nvim/lua/dko/plugins/init.lua
@@ -665,52 +665,52 @@ return {
   -- Noice
   -- =========================================================================
 
-  {
-    "folke/noice.nvim",
-    config = function()
-      require("noice").setup({
-        -- add any options here
-        routes = {
-          -- {
-          --   filter = {
-          --     event = "msg_show",
-          --     any = {
-          --       { find = "%d+L, %d+B" },
-          --       { find = "; after #%d+" },
-          --       { find = "; before #%d+" },
-          --       { find = "%d fewer lines" },
-          --       { find = "%d more lines" },
-          --     },
-          --   },
-          --   opts = { skip = true },
-          -- },
-        },
-        throttle = 1000 / 30, -- how frequently does Noice need to check for ui updates? This has no effect when in blocking mode.
-        -- lsp = {
-        --   -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        --   hover = {
-        --     enabled = true,
-        --   },
-        --   signature = {
-        --     enabled = true,
-        --   },
-        --   override = {
-        --     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        --     ["vim.lsp.util.stylize_markdown"] = true,
-        --     ["cmp.entry.get_documentation"] = true,
-        --   },
-        -- },
-      })
-    end,
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
-    },
-  },
+  -- {
+  --   "folke/noice.nvim",
+  --   config = function()
+  --     require("noice").setup({
+  --       -- add any options here
+  --       routes = {
+  --         -- {
+  --         --   filter = {
+  --         --     event = "msg_show",
+  --         --     any = {
+  --         --       { find = "%d+L, %d+B" },
+  --         --       { find = "; after #%d+" },
+  --         --       { find = "; before #%d+" },
+  --         --       { find = "%d fewer lines" },
+  --         --       { find = "%d more lines" },
+  --         --     },
+  --         --   },
+  --         --   opts = { skip = true },
+  --         -- },
+  --       },
+  --       throttle = 1000 / 30, -- how frequently does Noice need to check for ui updates? This has no effect when in blocking mode.
+  --       -- lsp = {
+  --       --   -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+  --       --   hover = {
+  --       --     enabled = true,
+  --       --   },
+  --       --   signature = {
+  --       --     enabled = true,
+  --       --   },
+  --       --   override = {
+  --       --     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+  --       --     ["vim.lsp.util.stylize_markdown"] = true,
+  --       --     ["cmp.entry.get_documentation"] = true,
+  --       --   },
+  --       -- },
+  --     })
+  --   end,
+  --   dependencies = {
+  --     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+  --     "MunifTanjim/nui.nvim",
+  --     -- OPTIONAL:
+  --     --   `nvim-notify` is only needed, if you want to use the notification view.
+  --     --   If not available, we use `mini` as the fallback
+  --     "rcarriga/nvim-notify",
+  --   },
+  -- },
 
   -- =========================================================================
   -- Neorg
