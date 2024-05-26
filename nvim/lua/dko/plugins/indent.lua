5b1d08d3386d540ab76b919c824ac1b374cd3b05
David O'Trakoun <me@davidosomething.com>
2024-05-25 23:54:44 -0400

    try indentmini again, cleanup hlchunk cfg

--- nvim/lua/dko/plugins/indent.lua
+++ nvim/lua/dko/plugins/indent.lua
@@ -4,36 +4,29 @@ local has_ui = #uis > 0
 --- @type 'hlchunk'|'indentmini'
 local enabled = "indentmini"
 
-local hlchunk_blank = false
-local hlchunk_chunk = true
-local hlchunk_indent = false
-
 return {
   -- indent guides
   -- every plugin has issues, leave a bunch of configs here and swapping as
   -- needed
 
-  -- =========================================================================
   -- https://github.com/nvimdev/indentmini.nvim
-  -- =========================================================================
   {
     "nvimdev/indentmini.nvim",
     cond = has_ui,
     enabled = enabled == "indentmini",
     event = "BufEnter",
     config = function()
-      require("indentmini").setup({
-        char = "█",
-        -- Indentmini gets janky when diffing php files
-        exclude = {
-          "php",
-        },
-      })
       local function color()
-        vim.cmd.highlight(("default IndentLine guifg=%s"):format(
-          -- require("dko.colors").is_dark() and "#242426" or "#f4f2ef"
-          require("dko.colors").is_dark() and "#003F4F"
-            or "#003F4F" -- #003542
+        vim.cmd.highlight("IndentLine guifg=bg")
+        -- vim.cmd.highlight(
+        --   ("IndentLine guifg=%s"):format(
+        --     require("dko.colors").is_dark() and "#242426" or "#f4f2ef"
+        --     require("dko.colors").is_dark() and "#003F4F" or "#003F4F"
+        --   )
+        -- )
+        vim.cmd.highlight(("IndentLineCurrent guifg=%s"):format(
+          -- require("dko.colors").is_dark() and "#344466" or "#c4c2df"
+          require("dko.colors").is_dark() and "#003F4F" or "#003F4F"
         ))
       end
       vim.api.nvim_create_autocmd("colorscheme", {
@@ -41,58 +34,55 @@ return {
         desc = "change indent guide colors with colorscheme",
       })
       color()
+
+      require("indentmini").setup({
+        -- char = "█",
+      })
     end,
   },
 
-  -- =========================================================================
   -- https://github.com/shellRaining/hlchunk.nvim
-  -- =========================================================================
-
   {
     "shellRaining/hlchunk.nvim",
     cond = has_ui,
     enabled = enabled == "hlchunk",
     event = "UIEnter",
     config = function()
-      local exclude_filetype = {
-        "help",
-        "plugin",
-        "alpha",
-        "dashboard",
-        "neo-tree",
-        "Trouble",
-        "lazy",
-        "mason",
-      }
+      -- local exclude_filetype = {
+      --   "help",
+      --   "plugin",
+      --   "alpha",
+      --   "dashboard",
+      --   "neo-tree",
+      --   "Trouble",
+      --   "lazy",
+      --   "mason",
+      -- }
 
-      require("hlchunk").setup({
-        blank = {
-          enable = hlchunk_blank,
-          exclude_filetype = exclude_filetype,
-          chars = { " " },
-          notify = false,
-          style = {
-            { bg = "", fg = "" },
-            {
-              bg = function()
-                -- return require("dko.colors").is_dark() and "#242426"
-                --   or "#f4f2ef"
-                return require("dko.colors").is_dark() and "#003F4F"
-                  or "#003F4F"
-              end,
-            },
-          },
+      -- local blank = require("hlchunk.mods.indent")
+      -- blank({
+      --   enable = hlchunk_blank,
+      --   exclude_filetype = exclude_filetype,
+      --   chars = { " " },
+      --   notify = false,
+      --   style = {
+      --     { bg = "", fg = "" },
+      --     {
+      --       bg = function()
+      --         return require("dko.colors").is_dark() and "#242426" or "#f4f2ef"
+      -- return require("dko.colors").is_dark() and "#003F4F" or "#003F4F"
+      --       end,
+      --     },
+      --   },
+      -- }):enable()
+
+      local chunk = require("hlchunk.mods.chunk")
+      chunk({
+        exclude_filetypes = {
+          sh = true,
         },
-        chunk = {
-          enable = hlchunk_chunk,
-          exclude_filetypes = {
-            sh = true,
-          },
-          notify = false,
-        },
-        indent = { enable = hlchunk_indent },
-        line_num = { enable = false },
-      })
+        notify = false,
+      }):enable()
     end,
   },
 }
