8014389702bc4d4c5088a29bf12467709464d9bf
David O'Trakoun <me@davidosomething.com>
2025-11-22 21:35:12 -0500

    font size down

--- wezterm/dko/typography.lua
+++ wezterm/dko/typography.lua
@@ -74,7 +74,7 @@ M.setup = function(config)
 
   config.font_dirs = { "fonts" }
 
-  config.font_size = 16.0
+  config.font_size = 12.0
 
   -- this is the default if using latest nightly
   --config.front_end = "WebGpu"
