davidosomething/dotfiles

jira-mine from alias to fn, lunette bind more

mac/hammerspoon/window/lunette.lua
@@ -7,28 +7,16 @@ hs.loadSpoon("Lunette")
 
 spoon.Lunette:bindHotkeys({
   cycleWidth = false,
-  leftHalf = {
-    { mc, "A" },
-  },
-  rightHalf = {
-    { mc, "D" },
-  },
-  topHalf = {
-    { mc, "K" },
-  },
-  bottomHalf = {
-    { mc, "J" },
-  },
-  topLeft = false,
-  topRight = false,
-  bottomLeft = false,
-  bottomRight = false,
-  fullScreen = {
-    { mc, "F" },
-  },
-  center = {
-    { mc, "S" },
-  },
+  leftHalf = { { mc, "A" } },
+  rightHalf = { { mc, "D" } },
+  topHalf = { { mc, "K" } },
+  bottomHalf = { { mc, "J" } },
+  topLeft = { { mc, "Q" } },
+  topRight = { { mc, "E" } },
+  bottomLeft = { { mc, "Z" } },
+  bottomRight = { { mc, "C" } },
+  fullScreen = { { mc, "F" } },
+  center = { { mc, "S" } },
   nextThird = false,
   prevThird = false,
   enlarge = false,

templeman/dotfiles-1

mac/hammerspoon/window/lunette.lua
@@ -7,26 +7,16 @@ hs.loadSpoon("Lunette")
 
 spoon.Lunette:bindHotkeys({
   cycleWidth = false,
-  leftHalf = {
-    { mc, "H" },
-  },
-  rightHalf = {
-    { mc, "L" },
-  },
-  topHalf = {
-    { mc, "K" },
-  },
-  bottomHalf = {
-    { mc, "J" },
-  },
-  topLeft = false,
-  topRight = false,
-  bottomLeft = false,
-  bottomRight = false,
-  fullScreen = {
-    { mc, "F" },
-  },
-  center = false,
+  leftHalf = { { mc, "H" } },
+  rightHalf = { { mc, "L" } },
+  topHalf = { { mc, "K" } },
+  bottomHalf = { { mc, "J" } },
+  topLeft = { { mc, "Q" } },
+  topRight = { { mc, "E" } },
+  bottomLeft = { { mc, "Z" } },
+  bottomRight = { { mc, "C" } },
+  fullScreen = { { mc, "F" } },
+  center = { { mc, "S" } },
   nextThird = false,
   prevThird = false,
   enlarge = false,