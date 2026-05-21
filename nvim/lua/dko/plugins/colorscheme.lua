davidosomething/dotfiles

giteditor bin with env for neovim read and pass as context to plugin specs disable filewatch and some plugins when in git editor

nvim/lua/dko/plugins/colorscheme.lua
@@ -1,32 +1,34 @@
 local dev = vim.env.NVIM_DEV ~= nil
 
----@type LazySpec
-return {
-  {
-    "davidosomething/vim-colors-meh",
-    cond = #vim.api.nvim_list_uis() > 0,
-    dependencies = {
-      -- { "rakr/vim-two-firewatch", lazy = true },
-      -- {
-      --   "mcchrish/zenbones.nvim",
-      --   lazy = true,
-      --   dependencies = { "rktjmp/lush.nvim" },
-      -- },
-      -- "ntk148v/komau.vim",
-      "oskarnurm/koda.nvim",
+return require("dko.utils.lazyspec")(function(ctx)
+  ---@type LazySpec
+  return {
+    {
+      "davidosomething/vim-colors-meh",
+      cond = ctx.has_ui,
+      dependencies = {
+        -- { "rakr/vim-two-firewatch", lazy = true },
+        -- {
+        --   "mcchrish/zenbones.nvim",
+        --   lazy = true,
+        --   dependencies = { "rktjmp/lush.nvim" },
+        -- },
+        -- "ntk148v/komau.vim",
+        "oskarnurm/koda.nvim",
+      },
+      dev = dev,
+      lazy = false,
+      priority = 1000,
+      init = function()
+        require("dko.settings").set("colors.dark", "meh")
+        require("dko.settings").set("colors.light", "koda-glade")
+      end,
+      config = function()
+        vim.cmd.colorscheme("meh")
+        if vim.env.TERM_PROGRAM == "WezTerm" then
+          require("dko.colors").wezterm_sync()
+        end
+      end,
     },
-    dev = dev,
-    lazy = false,
-    priority = 1000,
-    init = function()
-      require("dko.settings").set("colors.dark", "meh")
-      require("dko.settings").set("colors.light", "koda-glade")
-    end,
-    config = function()
-      vim.cmd.colorscheme("meh")
-      if vim.env.TERM_PROGRAM == "WezTerm" then
-        require("dko.colors").wezterm_sync()
-      end
-    end,
-  },
-}
+  }
+end)

templeman/dotfiles-1

nvim/lua/dko/plugins/colorscheme.lua
@@ -1,34 +1,35 @@
 local dev = vim.env.NVIM_DEV ~= nil
 
----@type LazySpec
-return {
-  {
-    --"davidosomething/vim-colors-meh",
-    "lifepillar/vim-solarized8",
-    branch = "neovim",
-    cond = #vim.api.nvim_list_uis() > 0,
-    dependencies = {
-      -- { "rakr/vim-two-firewatch", lazy = true },
-      -- {
-      --   "mcchrish/zenbones.nvim",
-      --   lazy = true,
-      --   dependencies = { "rktjmp/lush.nvim" },
-      -- },
-      -- "ntk148v/komau.vim",
-      "oskarnurm/koda.nvim",
+return require("dko.utils.lazyspec")(function(ctx)
+  ---@type LazySpec
+  return {
+    {
+      -- "davidosomething/vim-colors-meh",
+      "lifepillar/vim-solarized8",
+      cond = ctx.has_ui,
+      dependencies = {
+        -- { "rakr/vim-two-firewatch", lazy = true },
+        -- {
+        --   "mcchrish/zenbones.nvim",
+        --   lazy = true,
+        --   dependencies = { "rktjmp/lush.nvim" },
+        -- },
+        -- "ntk148v/komau.vim",
+        "oskarnurm/koda.nvim",
+      },
+      dev = dev,
+      lazy = false,
+      priority = 1000,
+      init = function()
+        require("dko.settings").set("colors.dark", "solarized8")
+        require("dko.settings").set("colors.light", "koda-glade")
+      end,
+      config = function()
+        vim.cmd.colorscheme("solarized8")
+        if vim.env.TERM_PROGRAM == "WezTerm" then
+          require("dko.colors").wezterm_sync()
+        end
+      end,
     },
-    dev = dev,
-    lazy = false,
-    priority = 1000,
-    init = function()
-      require("dko.settings").set("colors.dark", "solarized8")
-      require("dko.settings").set("colors.light", "koda-glade")
-    end,
-    config = function()
-      vim.cmd.colorscheme("solarized8")
-      if vim.env.TERM_PROGRAM == "WezTerm" then
-        require("dko.colors").wezterm_sync()
-      end
-    end,
-  },
-}
+  }
+end)